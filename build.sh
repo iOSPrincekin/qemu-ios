1511b067e36ad6d4ec0582e98a143d0740878a57
LEE <15267030696@163.com>
2023-07-09 21:59:01 +0800

    1.update;

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 op=$1
 
 if [ "$op" = "build" ]; then
+    mkdir -p build
     cd build
+    ../configure --enable-sdl --disable-cocoa --target-list=arm-softmmu --disable-capstone --disable-pie --disable-slirp --extra-cflags=-I/opt/homebrew/Cellar/openssl@3/3.1.0/include --extra-ldflags='-L/opt/homebrew/Cellar/openssl@3/3.1.0/lib -lcrypto'
     make
 elif [ "$op" = "run" ]; then
     echo "run"
